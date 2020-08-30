007a7b2e43f09cba3ce5e48ae022e821fba5ad23
Industrialspaghetti <54249275+Industrialspaghetti@users.noreply.github.com>
2020-08-30 14:31:40 -0700

    sodijfoidfi

---
+++ src/_01_algorithms/_1_prime_or_not/Prime.java
@@ -0,0 +1,10 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Prime {
+public static void main(String[] args) {
+	String number = JOptionPane.showInputDialog("please insert a number");
+	int number
+}
+}
