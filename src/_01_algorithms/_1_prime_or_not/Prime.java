6949d994d566265e63d795e18e990f2ea4b2db4a
Industrialspaghetti <54249275+Industrialspaghetti@users.noreply.github.com>
2020-09-06 14:32:05 -0700

    not much

--- src/_01_algorithms/_1_prime_or_not/Prime.java
+++ src/_01_algorithms/_1_prime_or_not/Prime.java
@@ -5,6 +5,10 @@ import javax.swing.JOptionPane;
 public class Prime {
 public static void main(String[] args) {
 	String number = JOptionPane.showInputDialog("please insert a number");
-	int number
+	int Number = Integer.parseInt(number);
+	
+	for (int i = 0; i < args.length; i++) {
+		
+	}
 }
 }
