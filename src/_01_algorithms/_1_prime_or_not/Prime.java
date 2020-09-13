dc87b124bc5af99d0b0630af62ab81adb91953d6
Industrialspaghetti <54249275+Industrialspaghetti@users.noreply.github.com>
2020-09-13 14:27:50 -0700

    Prime or not finished

--- src/_01_algorithms/_1_prime_or_not/Prime.java
+++ src/_01_algorithms/_1_prime_or_not/Prime.java
@@ -3,12 +3,21 @@ package _01_algorithms._1_prime_or_not;
 import javax.swing.JOptionPane;
 
 public class Prime {
-public static void main(String[] args) {
-	String number = JOptionPane.showInputDialog("please insert a number");
-	int Number = Integer.parseInt(number);
-	
-	for (int i = 0; i < args.length; i++) {
-		
+	public static void main(String[] args) {
+		String number = JOptionPane.showInputDialog("please insert a number");
+		int Number = Integer.parseInt(number);
+
+		for (int i = 2; i < Number; i++) {
+			if (Number % i == 0) {
+
+				System.out.println("number is not prime");
+
+				System.exit(0);
+
+			}
+
+		}
+		System.out.println("ur number is prime");
+
 	}
-}
 }
