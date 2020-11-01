c96a352b56ce8c5437a568eac3be9b068d989d2f
Industrialspaghetti <54249275+Industrialspaghetti@users.noreply.github.com>
2020-11-01 14:29:46 -0800

    worked on it

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Forloopguantlet.java
@@ -0,0 +1,18 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class Forloopguantlet {
+public static void main(String[] args) {
+	//1:
+	
+	for (int i = 0; i < 101; i++) {
+	System.out.println(i);	
+	
+	}
+	//2.
+	
+	for (int i = 100; i >= 0; i--) {
+		System.out.println(i);
+		
+	}
+}
+}
