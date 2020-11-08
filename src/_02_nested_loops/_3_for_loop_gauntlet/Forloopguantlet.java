9aa097be8512d4034c56732e7074bf0dcdae529d
Industrialspaghetti <54249275+Industrialspaghetti@users.noreply.github.com>
2020-11-08 14:32:34 -0800

    forloopguantlet

--- src/_02_nested_loops/_3_for_loop_gauntlet/Forloopguantlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Forloopguantlet.java
@@ -1,18 +1,45 @@
 package _02_nested_loops._3_for_loop_gauntlet;
 
 public class Forloopguantlet {
-public static void main(String[] args) {
-	//1:
+	public static void main(String[] args) {
+		// 1:
+
+		for (int i = 0; i < 101; i++) {
+			System.out.println(i);
+
+		}
+		// 2.
+
+		for (int i = 100; i >= 0; i--) {
+			System.out.println(i);
+
+		}
+		// 3.
+
+		for (int i = 2; i <= 100; i++) {
+
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+
+		}
+		for (int i = 1; i <= 99; i++) {
+
+			if (i % 2 == 1) {
+				System.out.println(i);
+			}
 	
-	for (int i = 0; i < 101; i++) {
-	System.out.println(i);	
-	
-	}
-	//2.
-	
-	for (int i = 100; i >= 0; i--) {
-		System.out.println(i);
+		}
+	for (int i = 1; i < 501; i++) {
+			
+			if( i % 2 == 0) {
+				System.out.println(i + "eve");
+			
+			}
+			if (i % 2 == 1) {
+				System.out.println(i +"odd");
+			}
+		}
 		
 	}
-}
 }
